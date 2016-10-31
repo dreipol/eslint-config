e04059d705ff913c26835638d2f06aaa4780efb6
Nils Solanki <nils.solanki@dreipol.ch>
2016-10-31 18:23:55 +0100

    Add support for object rest and spread

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
         "ecmaVersion": 7,
         "sourceType": "module",
         "ecmaFeatures": {
-            "impliedStrict": true
+            "impliedStrict": true,
+            "experimentalObjectRestSpread": true
         },
     },
     "env": {
