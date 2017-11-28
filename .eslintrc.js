b9a1458a82dff73df7737f5c7bc587b09d66200a
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2017-11-28 16:50:38 +0100

    Activate valid-jsdoc rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,13 @@ module.exports = {
     "no-unsafe-finally": "error",
     "no-unsafe-negation": "warn",
     "use-isnan": "error",
-    //"valid-jsdoc": "off",
+    "valid-jsdoc": ["warn", {
+      "prefer": { "arg": "param", "argument": "param", "returns": "return", "virtual": "abstract" },
+      "preferType": { "array": "Array", "Boolean": "boolean", "function": "Function", "Number": "number", "object": "Object", "String": "string" },
+      "requireReturn": false,
+      "requireReturnType": true,
+      "matchDescription": ".+",
+    }],
     "valid-typeof": "error",
 
     //# Best Practices
